dd6d6d36db57703b0a6621b787f0bb9f8182c332
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-04-19 21:27:07 +0800

    updated 26_ Remove_Duplicates_from_Sorted_Array

--- java/26_ Remove_Duplicates_from_Sorted_Array/src/removeduplicates/Remove_Duplicates.java
+++ java/26_ Remove_Duplicates_from_Sorted_Array/src/removeduplicates/Remove_Duplicates.java
@@ -5,16 +5,26 @@ import java.util.Arrays;
 public class Remove_Duplicates {
 
 	public static void main(String[] args) {
-		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 		Remove_Duplicates rd = new Remove_Duplicates();
 		System.out.println(rd.removeDuplicates(nums));
 		System.out.println(Arrays.toString(nums));
 	}
-    public int removeDuplicates(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-        	for(int j = 0; j < nums.length; j++) {
-        		
-        	}
-        }
-    }
+
+	public int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		int i = 0, j = 0, count = 0;
+		while (i < nums.length && j < nums.length) {
+			if (nums[i] == nums[j]) {
+				j++;
+			} else {
+				nums[count] = nums[j - 1];
+				i = j++;
+				count++;
+			}
+		}
+		nums[count] = nums[j - 1];
+		return ++count;
+	}
 }
