7f0195e2aa1b2440ff4038dc53b1ea7b21429754
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-04-20 17:34:16 +0800

    updated Implement_strStr.java

--- java/28_ Implement_strStr()/src/implement/Implement_strStr.java
+++ java/28_ Implement_strStr()/src/implement/Implement_strStr.java
@@ -7,22 +7,24 @@ import java.util.Map;
 public class Implement_strStr {
 
 	public static void main(String[] args) {
-		Map<String,String> maps = new HashMap<>();
+		Map<String, String> maps = new HashMap<>();
 		maps.put("hello", "ll");
-		maps.put("aaaa","bba");
-		maps.put("aaaaaaaaa","aaa");
-		Implement_strStr is =new Implement_strStr();
+		maps.put("aaaa", "bba");
+		maps.put("aaaaaaaaa", "aaa");
+		Implement_strStr is = new Implement_strStr();
 		Iterator<Map.Entry<String, String>> iterator = maps.entrySet().iterator();
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			Map.Entry<String, String> entry = iterator.next();
-			System.out.print("haystack=:"+entry.getKey()+" needle=:"+entry.getValue());
-			System.out.println(" result:"+is.strStr(entry.getKey(), entry.getValue()));
+			System.out.print("haystack=:" + entry.getKey() + " needle=:" + entry.getValue());
+			System.out.println(" result:" + is.strStr(entry.getKey(), entry.getValue()));
 		}
-		
+
 	}
+
 	public int strStr(String haystack, String needle) {
-		//haystack = haystack.substring(haystack.indexOf(needle.indexOf(i)), haystack.length());
-        return haystack.indexOf(needle);//。。。。。
-        
-    }
+		// haystack = haystack.substring(haystack.indexOf(needle.indexOf(i)),
+		// haystack.length());
+		return haystack.indexOf(needle);// 。。。。。
+
+	}
 }
