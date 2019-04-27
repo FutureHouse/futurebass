9194ffd8ed3b35a93872dc1f190b684a8b9af2c3
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-04-27 17:37:50 +0800

    updated 11_Container_With_Most_Water.

---
+++ java/11_Container_With_Most_Water/answer/Container_With_Most_Water.java
@@ -0,0 +1,29 @@
+package answer;
+
+public class Container_With_Most_Water {
+
+	public static void main(String[] args) {
+		int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+		System.out.print(maxArea(height));
+	}
+
+	public static int maxArea(int[] height) {
+		if (height == null)
+			return 0;
+		int left = 0, right = height.length - 1;
+		int result = 0;
+		while (left < right) {
+			if (height[left] < height[right]) {
+				int s = (right - left) * height[left];
+				result = result > s ? result : s;
+				left++;
+			} else {
+				int s = (right - left) * height[right];
+				result = result > s ? result : s;
+				right--;
+
+			}
+		}
+		return result;
+	}
+}
