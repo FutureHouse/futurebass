cad27741b373848284e5f002bd17d6217f8a6042
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-04-30 18:24:33 +0800

    updated 17_Letter_Combinations_of_a_Phone_Number.

---
+++ java/17_Letter_Combinations_of_a_Phone_Number/solution/Letter_Combinations.java
@@ -0,0 +1,37 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Letter_Combinations {
+
+	public static void main(String[] args) {
+		String digits = "23";
+		System.out.println(letterCombinations(digits).toString());
+
+	}
+
+	private static char[][] map = new char[][] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' },
+			{ 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' } };
+
+	public static List<String> letterCombinations(String digits) {
+		if (digits == null || digits.length() == 0) {
+			return new ArrayList<>();
+		}
+		List<String> li = new ArrayList<>();
+		List<String> result = new ArrayList<>();
+		result.add("");
+		char[] input = digits.toCharArray();
+		for (char ch : input) {
+			for (String s : result) {
+				for (char c : map[ch - '2']) {
+					li.add(s + c);
+				}
+			}
+			result = new ArrayList<>(li);
+			li.clear();
+		}
+
+		return result;
+	}
+}
