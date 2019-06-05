c0c9142979d97f87a647f811cc071cad6c053fe0
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-06-05 21:41:51 +0800

    updated 47_PermutationsII.

--- java/47_PermutationsII/solution/PermutationsII.java
+++ java/47_PermutationsII/solution/PermutationsII.java
@@ -1,18 +1,20 @@
 package solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Permutations {
+public class PermutationsII {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = { 1, 1, 3 };
-		Permutations per = new Permutations();
-		System.out.println(per.permute(nums).toString());
+		PermutationsII per = new PermutationsII();
+		System.out.println(per.permuteUnique(nums).toString());
 	}
 
-	public List<List<Integer>> permute(int[] nums) {
+	public List<List<Integer>> permuteUnique(int[] nums)  {
+		Arrays.sort(nums);
 		List<List<Integer>> result = new ArrayList<>();
 		List<Integer> numsList = new ArrayList<>();
 		List<Integer> resultLi = new ArrayList<>();
@@ -24,17 +26,18 @@ public class Permutations {
 	}
 
 	public void find(List<List<Integer>> result, List<Integer> numsList, List<Integer> resultLi) {
-		if(numsList.isEmpty()) {
+		if (numsList.isEmpty()) {
 			result.add(resultLi);
 			return;
 		}
-		for(int i =0;i<numsList.size();i++) {
+		
+		for (int i = 0; i < numsList.size(); i++) {
+			if (i>0 && numsList.get(i)==numsList.get(i-1)) continue;
 			List<Integer> numsListNew = new ArrayList<>(numsList);
 			List<Integer> resultLiNew = new ArrayList<>(resultLi);
 			resultLiNew.add(numsListNew.get(i));
 			numsListNew.remove(i);
-			find(result,numsListNew,resultLiNew);
+			find(result, numsListNew, resultLiNew);
 		}
 	}
-
 }
