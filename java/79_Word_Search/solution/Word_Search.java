43c772b1cc91eda7b545fa16fc7e2b2fc3290b92
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-06-25 17:11:52 +0800

    updated 80_Remove_Duplicates_from_Sorted_Array_II.

---
+++ java/79_Word_Search/solution/Word_Search.java
@@ -0,0 +1,33 @@
+package solution;
+
+public class Word_Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[][] board = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+		String word = "ABCB";
+		System.out.print(exist(board, word));
+	}
+
+	public static boolean exist(char[][] board, String word) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				if (word.indexOf(board[i][j]) == -1) {
+					board[i][j] = 0;
+				}
+			}
+		}
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				if (i == 0) {
+
+				} else if (j == 0) {
+
+				} else {
+
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ java/80_Remove_Duplicates_from_Sorted_Array_II/solution/Remove_Duplicates_from_Sorted_Array_II.java
@@ -0,0 +1,34 @@
+package solution;
+
+public class Remove_Duplicates_from_Sorted_Array_II {
+
+	public static void main(String[] args) {
+		int[] nums = {0,0,1,1,1,1,2,3,3,3};
+		// nums is passed in by reference. (i.e., without making a copy)
+		int len = removeDuplicates(nums);
+		System.out.println(len);
+		// any modification to nums in your function would be known by the caller.
+		// using the length returned by your function, it prints the first len elements.
+		for (int i = 0; i < len; i++) {
+		    System.out.print(nums[i]);
+		}
+	}
+	public static int removeDuplicates(int[] nums) {
+		int len = 0;
+		int index = 0;
+		for(int i = 2;i<nums.length-len;i++) {
+			if(nums[i]==nums[i-2]) {
+			index++;
+			}else if(index>0) {
+				len+=index;
+				i-=index;
+				for(int j = i ;j<nums.length-len;j++) {
+					nums[j]=nums[j+index];		
+				}
+				index=0;
+			}
+		}
+		
+        return nums.length -len-index;
+    }
+}
