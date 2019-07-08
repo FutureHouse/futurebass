c8710961e2a81c64e5ef232fc0791131981e1580
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-07-08 16:08:01 +0800

    224_Basic_Calculator.

--- java/224_Basic_Calculator/solution/Basic_Calculator.java
+++ java/224_Basic_Calculator/solution/Basic_Calculator.java
@@ -1,10 +1,42 @@
 package solution;
 
+import java.util.Stack;
+
 public class Basic_Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "(6+1)-(10+(5-5))";
+		System.out.print(calculate(s));
 
+	}
+
+	public static int calculate(String s) {
+		int result = 0, num = 0,sign = 1;
+		Stack<Integer> nums = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c >= '0' && c <= '9') {
+				num = num * 10 + c - '0';
+			} else if (c == '+') {
+				result += sign * num;
+				sign = 1;
+				num = 0;
+			} else if (c == '-') {
+				result += sign * num;
+				sign = -1;
+				num = 0;
+			} else if (c == '(') {
+				nums.push(result);
+				nums.push(sign);
+				sign =1;
+				result = 0;
+			} else if (c == ')') {
+				result += num * sign;
+				result *= nums.pop();
+				result += nums.pop();
+				num = 0;
+			}
+		}
+		return result + (sign * num);
 	}
 
 }
