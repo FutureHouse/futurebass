5408012b04cac5864d09a4bb2768535e759bf7f5
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-08-23 00:33:17 +0800

    updated 90_Subsets_II。

--- java/90_Subsets_II/solution/Subsets_II.java
+++ java/90_Subsets_II/solution/Subsets_II.java
@@ -1,29 +1,35 @@
 package solution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Subsets_II {
 
 	public static void main(String[] args) {
-		int[] nums = {1,2,2};
+		int[] nums = { 1, 2, 2 };
 		System.out.print(subsetsWithDup(nums));
 
 	}
-    public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> li =  new ArrayList<>();
-        find(nums,0,li, result);
-        return result;
-    }
-    
-    public static void find(int[] nums,int index,List<Integer> li, List<List<Integer>> result){
-    	for(int i=index;i<nums.length;i++) {
-    		li.add(nums[i]);
-    		find(nums,index+1,li, result);
-    		result.add(new ArrayList<>(li));
-    		li.clear();
-    		
-    	}
-    }
+
+	public static List<List<Integer>> subsetsWithDup(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> result = new ArrayList<>();
+		find(nums, 0, new ArrayList<Integer>(), result);
+		return result;
+	}
+
+	public static void find(int[] nums, int index, List<Integer> li, List<List<Integer>> result) {
+		result.add(new ArrayList<>(li));
+		for (int i = index; i < nums.length; i++) {
+			li.add(nums[i]);
+			find(nums, i + 1, li, result);
+			li.remove(li.size() - 1);
+			while (i + 1 < nums.length && nums[i + 1] == nums[i])
+				i++;
+		}
+
+	}
 }
