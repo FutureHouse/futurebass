4be70cee580202b090b73b84a2ce510bbe29bc07
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-09-04 19:35:26 +0800

    updated 106_Construct Binary Tree from Inorder and Postorder Traversal.

--- java/106_Construct Binary Tree from Inorder and Postorder Traversal/solution/ConstructBT.java
+++ java/106_Construct Binary Tree from Inorder and Postorder Traversal/solution/ConstructBT.java
@@ -3,36 +3,39 @@ package solution;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Construct {
+public class ConstructBT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private Map<Integer, Integer> map;
-	private int[] preorder;
-	private int pindex = 0;
+	Map<Integer, Integer> map;
+	int[] postorder;
+	int pindex;
 
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
 		map = new HashMap<>();
 		for (int i = 0; i < inorder.length; i++) {
 			map.put(inorder[i], i);
 		}
-		this.preorder = preorder;
-		return buildTree(0, preorder.length - 1);
+		this.postorder = postorder;
+		pindex = postorder.length - 1;
+		return buildTree(0, pindex);
 	}
 
-	public TreeNode buildTree(int start, int end) {
-		if (start > end)
+	private TreeNode buildTree(int start, int end) {
+		if (start > end) {
 			return null;
-		TreeNode cur = new TreeNode(preorder[pindex]);
-		pindex++;
+		}
+		TreeNode cur = new TreeNode(postorder[pindex]);
+		pindex--;
 		int inIndex = map.get(cur.val);
-		cur.left = buildTree(start, inIndex - 1);
 		cur.right = buildTree(inIndex + 1, end);
+		cur.left = buildTree(start, inIndex - 1);
 		return cur;
 	}
+
 }
 
 class TreeNode {
