d4e17267034b32b6e8b25fc813348ef5361e0edb
futurehouse <49586724+FutureHouse@users.noreply.github.com>
2019-09-29 00:28:02 +0800

    update

--- java/118_Pascal's Triangle/solution/PascalTriangle.java
+++ java/118_Pascal's Triangle/solution/PascalTriangle.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class PascalTriangle {
 
 	public static void main(String[] args) {
-		int numRows = 5;
-		System.out.print(generate(5));
+		int numRows = 3;
+		System.out.print(generate(numRows));
 	}
 
 	public static List<List<Integer>> generate(int numRows) {
-		List<List<Integer>> res = new ArrayList<>();
-		res.add(new ArrayList<>() {
-			{
-				add(1);
-			}
-		});
-		if(numRows==1)return res;
-		
-		return res;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> li = new ArrayList<>();
+        for(int i=0; i<numRows; i++) {
+            li.add(0, 1);
+            for(int j=1; j<li.size()-1; j++) {
+                li.set(j, li.get(j)+li.get(j+1));
+            }
+            ans.add(new ArrayList<Integer>(li));
+        }
+        return ans;
 	}
 }
